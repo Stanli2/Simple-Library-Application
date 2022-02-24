268ec2051a73669eea2f6c9cacd9e3a32e57a04d
Sley23 <obiwonike@gmail.com>
2022-02-25 00:39:06 +0100

    Latest Update

--- src/main/java/com/stanli/libraryapplication/service/serviceImplementation/BookServiceImpl.java
+++ src/main/java/com/stanli/libraryapplication/service/serviceImplementation/BookServiceImpl.java
@@ -5,9 +5,11 @@ import com.stanli.libraryapplication.dto.BookResponseDto;
 import com.stanli.libraryapplication.model.Book;
 import com.stanli.libraryapplication.repository.BookRepository;
 import com.stanli.libraryapplication.service.BookService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
@@ -18,16 +20,43 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookResponseDto save(BookRequestDto bookRequestDto) {
+        if (bookRequestDto != null){
+            Book book = new Book();
+
+            book.setBookTitle(bookRequestDto.getBookTitle());
+            book.setBookAuthor(bookRequestDto.getBookAuthor());
+            book.setCategory(bookRequestDto.getCategory());
+
+            Book book1 = bookRepository.save(book);
+            BookResponseDto bookResponseDto = new BookResponseDto(book1.getBookTitle(), book1.getBookAuthor(), book1.getCategory());
+
+            return bookResponseDto;
+        }
         return null;
     }
 
     @Override
-    public BookResponseDto editBook(BookRequestDto bookRequestDto) {
-        return null;
+    public BookResponseDto editBook(Long id, BookRequestDto bookRequestDto) {
+        Book book = bookRepository.getById(id);
+
+        book.setBookTitle(bookRequestDto.getBookTitle());
+        book.setBookAuthor(bookRequestDto.getBookAuthor());
+        book.setCategory(bookRequestDto.getCategory());
+
+        Book book1 = bookRepository.save(book);
+        BookResponseDto bookResponseDto = new BookResponseDto(book1.getBookTitle(), book1.getBookAuthor(), book1.getCategory());
+        return bookResponseDto;
+    }
+
+    @Override
+    public String deleteBook(Long id) {
+        bookRepository.deleteById(id);
+        return "Book Deleted";
     }
 
     @Override
     public List<Book> getAllBooks() {
-        return null;
+
+        return bookRepository.findAll();
     }
 }
