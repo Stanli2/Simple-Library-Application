95e221783c142f2574b16f381d7ae9649f7ea3f7
Sley23 <obiwonike@gmail.com>
2022-02-25 00:38:07 +0100

    Latest Update

--- src/main/java/com/stanli/libraryapplication/dto/BookResponseDto.java
+++ src/main/java/com/stanli/libraryapplication/dto/BookResponseDto.java
@@ -1,10 +1,20 @@
 package com.stanli.libraryapplication.dto;
 
 import com.stanli.libraryapplication.model.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Access;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookResponseDto {
 
     private String bookTitle;
     private String bookAuthor;
     private Category category;
+
+
 }
