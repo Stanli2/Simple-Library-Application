53194c053190a069d179ebbda1cc73b504719aa4
Sley23 <obiwonike@gmail.com>
2022-02-25 00:35:59 +0100

    Latest Update

--- src/main/java/com/stanli/libraryapplication/repository/BookRepository.java
+++ src/main/java/com/stanli/libraryapplication/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.stanli.libraryapplication.repository;
 
 import com.stanli.libraryapplication.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
