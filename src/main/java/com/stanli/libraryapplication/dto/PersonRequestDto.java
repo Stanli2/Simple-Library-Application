e6a0de280934fced25ca9e1d820740040d2d85d9
Sley23 <obiwonike@gmail.com>
2022-02-24 12:14:01 +0100

    Created Boilerplate

---
+++ src/main/java/com/stanli/libraryapplication/dto/PersonRequestDto.java
@@ -0,0 +1,12 @@
+package com.stanli.libraryapplication.dto;
+
+import com.stanli.libraryapplication.model.Role;
+
+public class PersonRequestDto {
+
+    private String username;
+    private String email;
+    private String password;
+    private String gender;
+    private Role role;
+}
