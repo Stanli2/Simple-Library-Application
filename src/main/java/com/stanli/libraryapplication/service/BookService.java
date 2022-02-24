6eff9e7c4c036633b2706a67b51de72f8e54c15f
Sley23 <obiwonike@gmail.com>
2022-02-25 00:38:38 +0100

    Latest Update

--- src/main/java/com/stanli/libraryapplication/service/BookService.java
+++ src/main/java/com/stanli/libraryapplication/service/BookService.java
@@ -3,14 +3,18 @@ package com.stanli.libraryapplication.service;
 import com.stanli.libraryapplication.dto.BookRequestDto;
 import com.stanli.libraryapplication.dto.BookResponseDto;
 import com.stanli.libraryapplication.model.Book;
+import org.springframework.stereotype.Service;
 
 import java.util.*;
 
+@Service
 public interface BookService {
 
     BookResponseDto save (BookRequestDto bookRequestDto);
 
-    BookResponseDto editBook (BookRequestDto bookRequestDto);
+    BookResponseDto editBook (Long id, BookRequestDto bookRequestDto);
+
+    String deleteBook (Long id);
 
     List<Book> getAllBooks ();
 }
