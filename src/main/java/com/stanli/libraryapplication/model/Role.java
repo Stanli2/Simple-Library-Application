0f428b15324969496ec10eddea4550a0317d5d33
Sley23 <obiwonike@gmail.com>
2022-02-25 08:41:01 +0100

    Latest Update

--- src/main/java/com/stanli/libraryapplication/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.stanli.libraryapplication.model;
-
-public enum Role {
-    LIBRARIAN,
-    USER
-}
--- src/main/java/com/stanli/libraryapplication/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stanli.libraryapplication.repository;
-
-import com.stanli.libraryapplication.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
